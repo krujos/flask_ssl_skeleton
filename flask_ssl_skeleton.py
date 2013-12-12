58c49f6341d6bc1653482b4283189156f9080b88
jkruck <joshua_kruck@symantec.com>
2013-12-12 10:02:55 -0600

    add logging as a demonstration.

--- flask_ssl_skeleton.py
+++ flask_ssl_skeleton.py
@@ -1,6 +1,7 @@
 from flask import Flask, g, request, redirect, session
 from flask.ext.auth.auth import SESSION_USER_KEY
 from flaskext.auth import Auth, AuthUser, login_required
+import logging
 
 
 Flask.get = lambda self, path: self.route(path, methods=['get'])
@@ -9,6 +10,7 @@ Flask.post = lambda self, path: self.route(path, methods=['post'])
 Flask.delete = lambda self, path: self.route(path, methods=['delete'])
 
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 auth = Auth(app)
 
 @app.before_request
@@ -21,14 +23,18 @@ def init_users():
 @app.get('/admin')
 @login_required()
 def admin():
-    return "Hello " + session.get(SESSION_USER_KEY, None)['username']
+    username = session.get(SESSION_USER_KEY, None)['username']
+    app.logger.debug(username + " accessed /admin")
+    return "Hello " + username
 
 @app.get('/')
 def index():
     user_key = session.get(SESSION_USER_KEY, None)
     if user_key:
+        app.logger.debug(user_key['username'] + " hit the root")
         return redirect('/admin')
 
+    app.logger.debug('giving back login form')
     return '''
             <form method="POST">
                 Username: <input type="text" name="username"/><br/>
@@ -44,6 +50,7 @@ def do_login():
     if username in g.users:
         # Authenticate and log in!
         if g.users[username].authenticate(request.form['password']):
+            app.logger.debug(username + " logged in!")
             return redirect('/admin')
     return 'Failure :('
 
