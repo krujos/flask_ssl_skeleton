ca250aa47286065019399ca01bd084d6112d4024
jkruck <joshua_kruck@symantec.com>
2013-12-13 08:56:55 -0600

    clean up unit tests use of constants

--- test/flask_ssl_skeleton_test.py
+++ test/flask_ssl_skeleton_test.py
@@ -26,6 +26,6 @@ class SkeletonTestCase(unittest.TestCase):
     @patch('flask_ssl_skeleton.User', autospec=True)
     def test_root_redirects_when_logged_in(self, f):
         with self.app.test_request_context():
-            rv = self.client.post('/', data=dict(username='admin', password='password'))
+            rv = self.client.post('/', data=dict(username=self.testuser, password=self.testpass))
             self.assertEquals(302, rv.status_code)
             self.assertTrue(rv.headers['Location'].endswith('/admin'))
